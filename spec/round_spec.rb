e6b94cb50d443792f9233b1935837bc4876b1eaf
Aurora Sanderson <AuroraTuring@gmail.com>
2023-12-28 17:48:04 -0700

    Add expects.

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -43,8 +43,35 @@ RSpec.describe Round do
     expect(round.turns.count).to eq(1)
   end
 
+  it "takes rounds" do 
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(round.turns).to eq()
+    expect(round.number_correct).to eq(1)
+    expect(round.current_card).to eq()
+    expect(round.take_turn("Venus")).to eq()
+    expect(round.turns.count).to eq(2)
+    expect(round.turns.last.feedback).to eq("Incorrect.")
+    expect(round.number_correct).to eq(1)
+    expect(round.number_correct_by_category(:Geography)).to eq(1)
+    expect(round.number_correct_by_category(:STEM)).to eq(0)
+    expect(round.percent_correct).to eq(50.0)
+    expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
+    expect(round.current_card).to eq()
+  end
+
 end
 
 
+pry(main)> round.turns
+#=> [#<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">]
 
+pry(main)> round.current_card
+#=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
+
+pry(main)> round.take_turn("Venus")
+#=> #<Turn:0x00007f972a215b38...>
+
+pry(main)> round.current_card
+#=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
 

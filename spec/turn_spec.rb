94d1ed5f189c62e62a8b01fef0c3454cde28d2e3
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-03 19:49:26 -0700

    Fix tabbing

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,18 +5,18 @@ require "./lib/turn"
 
 RSpec.describe Turn do
   it "exists" do
-   card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-   turn = Turn.new("Juneau", card)
-   expect(turn).to be_instance_of(Turn)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn).to be_instance_of(Turn)
   end
 
-  it "makes a guess" do 
+  it "makes a guess" do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     expect(turn.guess).to eq("Juneau")
-  end 
+  end
 
-  it "is correct" do 
+  it "is correct" do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     expect(turn.correct?).to be(true)
@@ -29,10 +29,10 @@ RSpec.describe Turn do
   end
 
 
-  it "guesses wrong" do 
+  it "guesses wrong" do
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn = Turn.new("Saturn", card)
-    expect(turn.guess).to eq("Saturn") 
+    expect(turn.guess).to eq("Saturn")
     expect(turn.correct?).to be(false)
     expect(turn.feedback).to eq("Incorrect.")
   end
